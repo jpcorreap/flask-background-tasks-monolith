e25fc88cd385ce952c5bc763c73f028d4c84cb78
casalazara <ca.salazara@uniandes.edu.co>
2022-02-11 23:16:38 -0500

    chore(base): added pre-commit

--- backend/settings.py
+++ backend/settings.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('PG_USER')}:{os.getenv('PG_PASSWORD')}@0.0.0.0:5432/{os.getenv('PG_DATABASE_NAME')}"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('PG_USER')}:{os.getenv('PG_PASSWORD')}@0.0.0.0:5432/{os.getenv('PG_DB_NAME')}"
 JWT_SECRET_KEY = os.getenv("SECRET")
 PROPAGATE_EXCEPTIONS = True
