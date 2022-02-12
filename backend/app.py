33476ac5d1da35cf2e1c7375224ba0aa659d4a7c
casalazara <ca.salazara@uniandes.edu.co>
2022-02-12 00:14:24 -0500

    feat(user): added sign up and sign in for admin

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from flask_restful import Api
 from models.model import db
 from schemas.schema import ma
 
+from backend.views.user import SignIn, SignUp
+
 # app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
 app = Flask(__name__)
 app.config.from_object("settings")
@@ -21,8 +23,8 @@ CORS(app)
 
 # api.add_resource(ResourceEvent, "/api/events")
 # api.add_resource(ResourceEventDetail, "/api/events/<int:id_event>")
-# api.add_resource(SignUp, "/api/signup")
-# api.add_resource(SignIn, "/api/signin")
+api.add_resource(SignUp, "/api/signup")
+api.add_resource(SignIn, "/api/signin")
 
 
 @app.route("/")
