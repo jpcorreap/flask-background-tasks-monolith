a4346a0f63d0cd7a04f25706e58385366dc34a28
casalazara <ca.salazara@uniandes.edu.co>
2022-02-14 20:59:00 -0500

    fix(signup): updated unique

--- backend/models/contest.py
+++ backend/models/contest.py
@@ -5,7 +5,7 @@ class Contest(db.Model):
     __tablename__ = "contest"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     url = db.Column(db.String(128), unique=True, nullable=False)
-    name = db.Column(db.String(128), unique=True, nullable=False)
+    name = db.Column(db.String(128), nullable=False)
     banner = db.Column(db.String(128), unique=True, nullable=False)
     start_date = db.Column(db.DateTime, nullable=False)
     end_date = db.Column(db.DateTime, nullable=False)
