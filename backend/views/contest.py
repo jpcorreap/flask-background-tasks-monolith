f3394baece74d9837547f13a03479c6e71df4fe4
casalazara <ca.salazara@uniandes.edu.co>
2022-02-14 21:46:23 -0500

    fix(all): exception handling

--- backend/views/contest.py
+++ backend/views/contest.py
@@ -7,6 +7,7 @@ from flask_restful import Resource
 from models.contest import Contest
 from models.model import db
 from schemas.contest import contest_schema, contests_schema
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class ResourceContest(Resource):
@@ -21,20 +22,24 @@ class ResourceContest(Resource):
 
     @jwt_required()
     def post(self):
-        new_contest = Contest(
-            url=request.json["url"],
-            name=request.json["name"],
-            banner=request.json["banner"],
-            start_date=datetime.strptime(request.json["start_date"], DATE_FORMAT),
-            end_date=datetime.strptime(request.json["end_date"], DATE_FORMAT),
-            prize=request.json["prize"],
-            script=request.json["script"],
-            advices=request.json["advices"],
-            admin=get_jwt_identity(),
-        )
-        db.session.add(new_contest)
-        db.session.commit()
-        return contest_schema.dump(new_contest)
+        try:
+            new_contest = Contest(
+                url=request.json["url"],
+                name=request.json["name"],
+                banner=request.json["banner"],
+                start_date=datetime.strptime(request.json["start_date"], DATE_FORMAT),
+                end_date=datetime.strptime(request.json["end_date"], DATE_FORMAT),
+                prize=request.json["prize"],
+                script=request.json["script"],
+                advices=request.json["advices"],
+                admin=get_jwt_identity(),
+            )
+            db.session.add(new_contest)
+            db.session.commit()
+            return contest_schema.dump(new_contest)
+        except SQLAlchemyError as e:
+            error = str(e.__dict__["orig"])
+            return error, 422
 
 
 class ResourceContestDetail(Resource):
@@ -44,19 +49,25 @@ class ResourceContestDetail(Resource):
 
     @jwt_required()
     def put(self, contest_url):
-        contest = Contest.query.filter_by(
-            url=contest_url, admin=get_jwt_identity()
-        ).first_or_404()
-        contest.url = request.json["url"]
-        contest.name = request.json["name"]
-        contest.banner = request.json["banner"]
-        contest.start_date = datetime.strptime(request.json["start_date"], DATE_FORMAT)
-        contest.end_date = datetime.strptime(request.json["end_date"], DATE_FORMAT)
-        contest.prize = request.json["prize"]
-        contest.script = request.json["script"]
-        contest.advices = request.json["advices"]
-        db.session.commit()
-        return contest_schema.dump(contest)
+        try:
+            contest = Contest.query.filter_by(
+                url=contest_url, admin=get_jwt_identity()
+            ).first_or_404()
+            contest.url = request.json["url"]
+            contest.name = request.json["name"]
+            contest.banner = request.json["banner"]
+            contest.start_date = datetime.strptime(
+                request.json["start_date"], DATE_FORMAT
+            )
+            contest.end_date = datetime.strptime(request.json["end_date"], DATE_FORMAT)
+            contest.prize = request.json["prize"]
+            contest.script = request.json["script"]
+            contest.advices = request.json["advices"]
+            db.session.commit()
+            return contest_schema.dump(contest)
+        except SQLAlchemyError as e:
+            error = str(e.__dict__["orig"])
+            return error, 422
 
     @jwt_required()
     def delete(self, contest_url):
@@ -69,24 +80,28 @@ class ResourceContestDetail(Resource):
 
     @jwt_required()
     def patch(self, contest_url):
-        contest = Contest.query.filter_by(
-            url=contest_url, admin=get_jwt_identity()
-        ).first_or_404()
-        if url := request.json.get("url"):
-            contest.url = url
-        if name := request.json.get("name"):
-            contest.name = name
-        if banner := request.json.get("banner"):
-            contest.banner = banner
-        if prize := request.json.get("prize"):
-            contest.prize = prize
-        if script := request.json.get("script"):
-            contest.script = script
-        if advices := request.json.get("advices"):
-            contest.advices = advices
-        if start_date := request.json.get("start_date"):
-            contest.start_date = start_date
-        if end_date := request.json.get("end_date"):
-            contest.end_date = end_date
-        db.session.commit()
-        return contest_schema.dump(contest)
+        try:
+            contest = Contest.query.filter_by(
+                url=contest_url, admin=get_jwt_identity()
+            ).first_or_404()
+            if url := request.json.get("url"):
+                contest.url = url
+            if name := request.json.get("name"):
+                contest.name = name
+            if banner := request.json.get("banner"):
+                contest.banner = banner
+            if prize := request.json.get("prize"):
+                contest.prize = prize
+            if script := request.json.get("script"):
+                contest.script = script
+            if advices := request.json.get("advices"):
+                contest.advices = advices
+            if start_date := request.json.get("start_date"):
+                contest.start_date = start_date
+            if end_date := request.json.get("end_date"):
+                contest.end_date = end_date
+            db.session.commit()
+            return contest_schema.dump(contest)
+        except SQLAlchemyError as e:
+            error = str(e.__dict__["orig"])
+            return error, 422
--- backend/views/submission.py
+++ backend/views/submission.py
@@ -4,6 +4,7 @@ import os
 import uuid
 
 from flask import config, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restful import Resource
 from models.contest import Contest
 from models.model import db
@@ -20,32 +21,35 @@ def allowed_file(filename):
 
 
 class ResourceSubmission(Resource):
+    @jwt_required(optional=True)
     def get(self, contest_url):
         contest = Contest.query.filter_by(url=contest_url).first_or_404()
-        submissions = (
-            Submission.query.filter_by(contest_id=contest.id)
-            .order_by(Submission.upload_date.desc())
-            .get_or_404()
-        )
+        if get_jwt_identity():
+            submissions = (
+                Submission.query.filter_by(contest_id=contest.id)
+                .order_by(Submission.upload_date.desc())
+                .get_or_404()
+            )
+        else:
+            submissions = (
+                Submission.query.filter_by(contest_id=contest.id, status="converted")
+                .order_by(Submission.upload_date.desc())
+                .get_or_404()
+            )
         return submissions_schema.dump(submissions)
 
     def post(self, contest_url):
         res = None
         if "file" not in request.files:
             res = ("not file in request", 400)
-
         if "data" not in request.files:
             res = ("not user data in request", 400)
-
         file = request.files["file"]
-
         if file.filename == "":
             res = ("no file founded in request", 400)
-
         json_data = json.load(request.files["data"])
         contest = Contest.query.filter_by(url=contest_url).first_or_404()
         user = User.query.filter_by(email=json_data["email"]).first()
-
         if not user:
             new_user = User(
                 email=json_data["email"],
@@ -54,9 +58,7 @@ class ResourceSubmission(Resource):
             )
             db.session.add(new_user)
             db.session.flush()
-
         new_submission: Submission
-
         if file and allowed_file(file.filename):
             file_id = uuid.uuid4()
             file_type = secure_filename(file.filename).split(".")[1]
