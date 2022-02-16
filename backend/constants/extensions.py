fdc3f6a9a13b826e04aa0724890199c1f637ee29
Jonatan Hernandez <47890623+jehernandezr@users.noreply.github.com>
2022-02-16 01:25:52 -0500

    feat(audio): audio get working (#10)
    * fix(settings): changed settings are loaded
    
    * fix(contest): changed the way banner is stored
    
    * fix(files): testing if creates dirs
    
    * fix(files): testing if creates dirs v2
    
    * fix(files): testing if creates dirs v3
    
    * fix(files): testing if creates dirs v3
    
    * fix(files): testing if creates dirs v4
    
    * fix(user): set id unique
    
    * fix(submission): removed /
    
    * fix(submission): changed path, added /submissions
    
    * fix(submission): changed path, added /submissions for detail
    
    * fix(submission): fixed user bug
    
    * fix(submission): fixed get bug
    
    * Update P1.postman_collection.json
    
    * fix(banner): changed the way banner is stored
    
    * fix(banner): uuid set in submission
    
    * fix(banner): uuid set in submission
    
    * fix(banner): changed unique constraint
    
    * fix(banner): changed uuid in submissions
    
    * feat(deploy): added necessary libs and files to deploy
    
    * feat(audio): returning audio file
    
    Co-authored-by: casalazara <ca.salazara@uniandes.edu.co>
    Co-authored-by: je.hernandezr <je.hernandezr@ISIS4426-5208.sis.virtual.uniandes.edu.co>

--- backend/constants/extensions.py
+++ backend/constants/extensions.py
@@ -1,4 +1,12 @@
 ALLOWED_EXTENSIONS = {
-    "audio": ["mp3", "wav", "m4a", "mpc", "wma"],
+    "audio": ["mp3", "wav", "m4a", "mpc", "wma", "flac", "mp4", "aac", "oga"],
     "image": ["jpg", "png"],
 }
+
+MAPPER_AUDIO_FILE = {
+    "mp3":"audio/mpeg",
+    "wav":"audio/x-wav",
+    "acc":"audio/aac",
+    "oga":"audio/ogg",
+    "m4a":"audio/mp4a-latm",
+} 
