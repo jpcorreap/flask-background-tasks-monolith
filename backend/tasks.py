a81fdd9fb35c030a839255ea502e434f7f0cd006
casalazara <ca.salazara@uniandes.edu.co>
2022-02-17 22:42:21 -0500

    feat(raw audio): added raw audio

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,15 +1,10 @@
-import os
 from celery import Celery
-from utils.db import db_session
 from custom_email.email_sender import send_many_emails
 import ffmpeg
-from models.admin import Admin
-from models.contest import Contest
 from models.submission import Submission, SubmissionStatus
-from models.user import User 
-
-
+from models.user import User
 from settings import config
+from utils.db import db_session
 
 app = Celery("tasks", broker="redis://localhost:6379/0")
 
@@ -29,31 +24,36 @@ def convert_to_mp3(filename: str):
     print("\n-> Se va a convertir el archivo: {}".format(filename))
     name = filename.split(".")[0]
     input = ffmpeg.input(
-        "{fpath}/{filename}".format(fpath=config.PROCESSING_FOLDER_PATH, filename=filename)
+        "{fpath}/{filename}".format(
+            fpath=config.PROCESSING_FOLDER_PATH, filename=filename
+        )
     )
     ffmpeg.output(
         input,
-        "{new_path}/{name}.mp3".format(new_path=config.PROCESSED_FOLDER_PATH, name=name),
+        "{new_path}/{name}.mp3".format(
+            new_path=config.PROCESSED_FOLDER_PATH, name=name
+        ),
         format="mp3",
     ).run(overwrite_output=True)
-    os.remove(
-        "{fpath}/{filename}".format(fpath=config.PROCESSING_FOLDER_PATH, filename=filename)
+    print(
+        "\n-> El archivo convertir se copió a : {}".format(config.PROCESSED_FOLDER_PATH)
     )
-    print("\n-> El archivo convertir se copió a : {}".format(config.PROCESSED_FOLDER_PATH))
 
 
 @app.task(base=SqlAlchemyTask)
 def process_audio_files():
     pending_submissions = (
-        db_session.query(Submission,User)
+        db_session.query(Submission, User)
         .filter_by(status=SubmissionStatus.processing)
         .join(User.submissions)
         .all()
     )
 
     if pending_submissions:
-        for (submission,user) in pending_submissions:
-            filename = "{id}.{file_type}".format(id=submission.id, file_type=submission.file_type)
+        for (submission, _) in pending_submissions:
+            filename = "{id}.{file_type}".format(
+                id=submission.id, file_type=submission.file_type
+            )
             try:
                 convert_to_mp3(filename)
                 submission.status = SubmissionStatus.converted
@@ -61,14 +61,16 @@ def process_audio_files():
                 print(e)
 
         converted_submissions = list(
-             filter(
-                lambda xy: (xy[0].status == SubmissionStatus.converted), pending_submissions)
+            filter(
+                lambda xy: (xy[0].status == SubmissionStatus.converted),
+                pending_submissions,
+            )
         )
 
-        db_session.add_all(  map(lambda xy:xy[0], converted_submissions))
+        db_session.add_all(map(lambda xy: xy[0], converted_submissions))
         db_session.commit()
 
-        users_emails = [user.email for _,user in converted_submissions]
+        users_emails = [user.email for _, user in converted_submissions]
 
         send_many_emails(
             users_emails, "Your Submission has been converted successfully"
