c166bbb73056da19791589e6b076f8a8db954c6f
Jonatan Hernandez <je.hernandezr@uniandes.edu.co>
2022-03-10 01:54:49 -0500

    email sendgrid

--- backend/custom_email/email_sender.py
+++ backend/custom_email/email_sender.py
@@ -2,6 +2,10 @@ import smtplib
 import ssl
 from typing import List
 from settings import config
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
 
 
 context = ssl.create_default_context()
@@ -11,13 +15,18 @@ def send_email(to: str, message: str):
       with smtplib.SMTP(
         config.EMAIL_SERVER_HOST, config.EMAIL_SERVER_PORT
     ) as server:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = 'Status Submission Update SuperVoices'
+        msg['From'] = formataddr(('Super Voices', 'juanpa1202@gmail.com'))
+        msg['To'] = to
+        msg.attach(MIMEText(message, 'plain'))
         server.ehlo()
         server.starttls(context=context)
         server.login(config.EMAIL_USER, config.EMAIL_PASSWORD)
         server.sendmail(
             config.EMAIL_USER,
             to,
-            message,
+            msg.as_string(),
         )
 
 
@@ -30,8 +39,13 @@ def send_many_emails(to: List[str], message: str):
         server.starttls(context=context)
         server.login(config.EMAIL_USER, config.EMAIL_PASSWORD)
         for receiver in to:
+            msg = MIMEMultipart('alternative')
+            msg['Subject'] = 'Status Submission Update SuperVoices'
+            msg['From'] = formataddr(('Super Voices', 'juanpa1202@gmail.com'))
+            msg['To'] = receiver
+            msg.attach(MIMEText(message, 'plain'))
             server.sendmail(
                 config.EMAIL_USER,
                 receiver,
-                message,
+                msg.as_string(),
             )
