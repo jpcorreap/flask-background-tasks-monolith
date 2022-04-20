2b7189dac87a396b1610faabf5057724d14d9dd6
Juan Pablo Correa <juanpablocorreapuerta@gmail.com>
2022-04-19 22:42:49 -0500

    Credentials commented

--- backend/utils/s3fs_utils.py
+++ backend/utils/s3fs_utils.py
@@ -5,11 +5,11 @@ BUCKET_NAME = "test-python-boto3-cloud"
 
 def get_client():
     return s3fs.S3FileSystem(
-        key="ASIA5TXLE3M6TDNTCRMN",
-        secret="av+gPloLvgdNUHUPd7OFH779lxB+KV0QNTwRC+lS",
-        token="FwoGZXIvYXdzEGIaDKaSszLKUkSNMjaKqCLKAW++UD/W57UT4uXxLhncIuulYWEWmfKEjadff/wj8DV9K4ORhL93vieRWHAtdMXpDrcIE4YZSVhLPmO6SXDdrtjSwewuHR/PJG2l4cOiuS7EJCH13mm3FVfoB2lJNzx6d/2xRK4U3J/RMbv8gUpYNSmDTtA6qG75Itp4wPieM8jCrO7vBcGAS5UfAMsfGOyDaPcOgwo3kGa/AQpL2EQn0cdAAN6BHsAq3wj67gCAtNUsiwELj/6aLknkdW2+vCvhXOXhg5HVQdkNfkQorLD9kgYyLaXQ8sAeEVDbUuaFvx62Jjq/d2mFkw3bL+5IEPumMWu+K0347ECeUTdwnUL1lA=="
+        # key="ASIA5TXLE3M6TDNTCRMN",
+        # secret="av+gPloLvgdNUHUPd7OFH779lxB+KV0QNTwRC+lS",
+        # token="FwoGZXIvYXdzEGIaDKaSszLKUkSNMjaKqCLKAW++UD/W57UT4uXxLhncIuulYWEWmfKEjadff/wj8DV9K4ORhL93vieRWHAtdMXpDrcIE4YZSVhLPmO6SXDdrtjSwewuHR/PJG2l4cOiuS7EJCH13mm3FVfoB2lJNzx6d/2xRK4U3J/RMbv8gUpYNSmDTtA6qG75Itp4wPieM8jCrO7vBcGAS5UfAMsfGOyDaPcOgwo3kGa/AQpL2EQn0cdAAN6BHsAq3wj67gCAtNUsiwELj/6aLknkdW2+vCvhXOXhg5HVQdkNfkQorLD9kgYyLaXQ8sAeEVDbUuaFvx62Jjq/d2mFkw3bL+5IEPumMWu+K0347ECeUTdwnUL1lA=="
         # Accessing all buckets you have access to with your credentials
-        # anon=False
+        anon=False
     )
 
 
